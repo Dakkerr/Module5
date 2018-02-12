a30491f10358c988e06de0fe6539b15d820c67c2
Dakkerr <dakotahgoetz@mail.usf.edu>
2018-02-11 22:48:47 -0500

    First Commit

---
+++ Module5F.R
@@ -0,0 +1,18 @@
+A = matrix(1:100, nrow=5)
+B = matrix(1:1000, nrow=5)
+t(A)
+t(B)
+a = c(1:2)
+b = c(1:4)
+X = a*A
+Y=b*B
+
+a=c(1:17)
+b=c(1:167)
+
+A = matrix(1:4, nrow = 2)
+solve(A)
+
+A = matrix(runif(25, min = 0, max = 50), nrow = 5)
+solve(A)
+det(A)
